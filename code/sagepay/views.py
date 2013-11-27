8cfed6fa67141d6868a38fa0c1accedcc0741b27
alex <alessandro@u-dox.com>
2013-11-27 09:53:40 +0000

    added mixins

--- code/sagepay/views.py
+++ code/sagepay/views.py
@@ -18,7 +18,7 @@ from oscar.apps.checkout.views import PaymentDetailsView as OscarPaymentDetailsV
 from oscar.apps.checkout.views import ThankYouView as OscarThankYouView
 from oscar.apps.checkout.views import ShippingAddressView as OscarShippingAddressView
 from oscar.apps.checkout.views import CheckoutSessionMixin
-from checkout.mixins import OrderPlacementMixin
+from .mixins import OrderPlacementMixin
 from oscar.apps.payment import models
 
 Basket = get_model('basket', 'Basket')
